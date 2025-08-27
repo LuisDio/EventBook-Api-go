7403a6794eaafc41b241e9a80b7c34ace9b2de04
LuisDio <luis.diolindo@gmail.com>
2025-08-27 14:20:52 +0200

    Added login endpoint and validation

--- models/user.go
+++ models/user.go
@@ -1,6 +1,8 @@
 package models
 
 import (
+	"errors"
+
 	"example.com/restapi-dev/db"
 	"example.com/restapi-dev/utils"
 )
@@ -36,4 +38,24 @@ func (u User) Save() error {
 	userId, err := result.LastInsertId()
 	u.ID = userId
 	return err
+}
+
+func (u User) ValidateCredentials() error {
+	query := "SELECT password FROM users WHERE email = ?"
+	row := db.DB.QueryRow(query, u.Email)
+
+	var retrievedPassword string
+	err := row.Scan(&retrievedPassword)
+
+	if err != nil {
+		return errors.New("Credentials invalid")
+	}
+
+	passwordIsValid := utils.CheckPasswordHash(u.Password, retrievedPassword)
+
+	if !passwordIsValid {
+		return errors.New("Credentials invalid")
+	}
+
+	return nil
 }
