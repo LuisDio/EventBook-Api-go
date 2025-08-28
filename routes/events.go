a36b7599e7df31263320ca9e5a876448deec5dd0
LuisDio <luis.diolindo@gmail.com>
2025-08-28 15:47:58 +0200

    verify token and protect create-event handler

--- routes/events.go
+++ routes/events.go
@@ -5,6 +5,7 @@ import (
 	"strconv"
 
 	"example.com/restapi-dev/models"
+	"example.com/restapi-dev/utils"
 	"github.com/gin-gonic/gin"
 )
 
@@ -31,8 +32,21 @@ func getEvent(context *gin.Context) {
 }
 
 func createEvent(context *gin.Context) {
+
+	token := context.Request.Header.Get("Authorization")
+	if token == "" {
+		context.JSON(http.StatusUnauthorized, gin.H{"message": "Not authorized"})
+	}
+
+	err := utils.VerifyToken(token)
+
+	if err != nil {
+		context.JSON(http.StatusUnauthorized, gin.H{"message": "Not authorized."})
+		return
+	}
+
 	var event models.Event
-	err := context.ShouldBindJSON(&event)
+	err = context.ShouldBindJSON(&event)
 
 	if err != nil {
 		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse request data"})
--- routes/routes.go
+++ routes/routes.go
@@ -5,9 +5,9 @@ import "github.com/gin-gonic/gin"
 func RegisterRoutes(server *gin.Engine) {
 	server.GET("/events", getEvents)
 	server.GET("/events/:id", getEvent)
-	server.POST("/events", createEvent)
-	server.PUT("/events/:id", updateEvent)
-	server.DELETE("/events/:id", deleteEvent)
+	server.POST("/events", createEvent)       // should be protected
+	server.PUT("/events/:id", updateEvent)    // should be protected
+	server.DELETE("/events/:id", deleteEvent) // should be protected
 	server.POST("/signup", signup)
 	server.POST("/login", login)
 }
